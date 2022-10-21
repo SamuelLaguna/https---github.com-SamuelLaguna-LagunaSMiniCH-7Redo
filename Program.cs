930e3ce6a2b7797533e6121470a0e48bc3399179
SamuelLaguna <samanater11@gmail.com>
2022-10-21 10:28:25 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿bool playAgain = true;
+bool end = false;
+Console.Clear();
+while(playAgain == true){
+int[] intArray = { 0, 1, 2, 3, 4, 5};
+Console.WriteLine(intArray.Length);
+Console.WriteLine(intArray[0]);
+
+
+for(int i = 0 ; i < intArray.Length; ++i){
+    Console.WriteLine(i);
+}
+
+int[] Array = { 0, 1, 2, 3, 4, 5};
+Console.WriteLine(intArray.Length);
+Console.WriteLine(intArray[0]);
+
+
+for (int i = intArray.Length - 1; i >= 0; i--){
+    Console.WriteLine(i);
+}
+    Console.WriteLine("To restart type playAgain to end type in end");
+    string ending = Console.ReadLine();
+        if(ending == "end")
+        {
+            playAgain = false;
+
+        }else
+        {
+            end = true;
+        }
+}
